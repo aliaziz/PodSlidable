695626ee41b82674da74b8aebb12e00bd0ba7a4a
Ziwa Ali <aziwa1991@gmail.com>
2018-12-19 19:05:41 +0300

    adds pods

---
+++ Slidable/Slidable.h
@@ -0,0 +1,19 @@
+//
+//  Slidable.h
+//  Slidable
+//
+//  Created by ali ziwa on 19/12/2018.
+//  Copyright © 2018 ali ziwa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Slidable.
+FOUNDATION_EXPORT double SlidableVersionNumber;
+
+//! Project version string for Slidable.
+FOUNDATION_EXPORT const unsigned char SlidableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Slidable/PublicHeader.h>
+
+
